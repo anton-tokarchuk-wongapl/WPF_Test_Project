75530a8e8c044b20e74dbae63af0dda20e24f42b
Anton Tokarchuk <atokar@softserveinc.com>
2019-08-20 12:25:07 +0300

    Create DTO layer.

---
+++ src/DTO/DTOs/ContentBaseDTOs/ContentBaseDTO.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DTO.DTOs.ContentBaseDTOs
+{
+    public class ContentBaseDTO
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public DateTime LastChangedDate { get; set; }
+
+        public virtual ICollection<ContentBaseDTO> Children { get; set; }
+
+        public int? ParentContentItemId { get; set; }
+
+        public virtual ContentBaseDTO ParentContentItem { get; set; }
+    }
+}
---
+++ src/DTO/DTOs/ContentBaseDTOs/ContentFileDTO.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DTO.DTOs.ContentBaseDTOs
+{
+    class ContentFileDTO
+    {
+    }
+}
