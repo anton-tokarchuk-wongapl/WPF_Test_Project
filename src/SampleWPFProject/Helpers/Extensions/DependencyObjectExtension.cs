c5587a6f55f790a05538924fb0a2be2e52d1a353
Anton Tokarchuk <atokar@softserveinc.com>
2019-09-03 13:34:41 +0300

    Clean and Update Extensions classes.

--- src/SampleWPFProject/Helpers/Extensions/DependencyObjectExtension.cs
+++ src/SampleWPFProject/Helpers/Extensions/DependencyObjectExtension.cs
@@ -5,70 +5,64 @@ using WPFProject.Helpers.Behaviours;
 
 namespace WPFProject.Helpers.Extensions
 {
-  /// <summary>
-  /// Extension methods fo the DependencyObject objects
-  /// </summary>
-  public static class DependencyObjectExtension
-  {
     /// <summary>
-    /// Find a sequence of children of type T
+    /// Extension methods fo the DependencyObject objects
     /// </summary>
-    /// <typeparam name="T"></typeparam>
-    /// <param name="control"></param>
-    /// <returns></returns>
-    public static T ParentOfType<T>(this DependencyObject control) where T : DependencyObject
+    public static class DependencyObjectExtension
     {
-      return ParentOfType<T>(control, null);
-    }
+        /// <summary>
+        /// Find a sequence of children of type T
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="control"></param>
+        /// <returns></returns>
+        public static T ParentOfType<T>(this DependencyObject control) where T : DependencyObject
+            => ParentOfType<T>(control, null);
 
-    /// <summary>
-    /// Find a sequence of children of type T and apply filter if applicable
-    /// </summary>
-    /// <typeparam name="T"></typeparam>
-    /// <param name="control"></param>
-    /// <param name="filter"></param>
-    /// <returns></returns>
-    public static T ParentOfType<T>(this DependencyObject control, Predicate<T> filter) where T : DependencyObject
-    {
-      var parent = VisualTreeHelper.GetParent(control);
+        /// <summary>
+        /// Find a sequence of children of type T and apply filter if applicable
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="control"></param>
+        /// <param name="filter"></param>
+        /// <returns></returns>
+        public static T ParentOfType<T>(this DependencyObject control, Predicate<T> filter) where T : DependencyObject
+        {
+            var parent = VisualTreeHelper.GetParent(control);
+            var t = parent as T;
 
-      var t = parent as T;
-      return t != null && (filter == null || filter(t))
-               ? t
-               : (parent != null ? parent.ParentOfType(filter) : null);
-    }
+            return t != null && (filter == null || filter(t))
+                     ? t
+                     : (parent != null ? parent.ParentOfType(filter) : null);
+        }
 
-    /// <summary>
-    /// 
-    /// </summary>
-    /// <param name="obj"></param>
-    /// <returns></returns>
-    public static bool IsEditing(this DependencyObject obj)
-    {
-      return TreeViewInPlaceEditBehavior.GetIsEditing(obj);
-    }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public static bool IsEditing(this DependencyObject obj)
+            => TreeViewInPlaceEditBehavior.GetIsEditing(obj);
 
-    /// <summary>
-    /// 
-    /// </summary>
-    /// <param name="obj"></param>
-    /// <returns></returns>
-    public static void BeginEdit(this DependencyObject obj)
-    {
-      TreeViewInPlaceEditBehavior.SetIsEditing(obj, true);
-    }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public static void BeginEdit(this DependencyObject obj)
+            => TreeViewInPlaceEditBehavior.SetIsEditing(obj, true);
 
-    /// <summary>
-    /// 
-    /// </summary>
-    /// <param name="obj"></param>
-    /// <param name="cancel"></param>
-    /// <returns></returns>
-    public static void EndEdit(this DependencyObject obj, bool cancel = true)
-    {
-      TreeViewInPlaceEditBehavior.SetIsEditCancelled(obj, cancel);
-      TreeViewInPlaceEditBehavior.SetIsEditConfirmed(obj, !cancel);
-      TreeViewInPlaceEditBehavior.SetIsEditing(obj, false);
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <param name="cancel"></param>
+        /// <returns></returns>
+        public static void EndEdit(this DependencyObject obj, bool cancel = true)
+        {
+            TreeViewInPlaceEditBehavior.SetIsEditCancelled(obj, cancel);
+            TreeViewInPlaceEditBehavior.SetIsEditConfirmed(obj, !cancel);
+            TreeViewInPlaceEditBehavior.SetIsEditing(obj, false);
+        }
     }
-  }
 }
--- src/SampleWPFProject/Helpers/Extensions/FrameworkElementExtension.cs
+++ src/SampleWPFProject/Helpers/Extensions/FrameworkElementExtension.cs
@@ -2,27 +2,24 @@ using System.Windows;
 
 namespace WPFProject.Helpers.Extensions
 {
-  /// <summary>
-  /// 
-  /// </summary>
-  public static class FrameworkElementExtension
-  {
-    public static void UpdateBindingTarget(this FrameworkElement element, DependencyProperty property)
+    public static class FrameworkElementExtension
     {
-      var bindingExpr = element.GetBindingExpression(property);
-      if (bindingExpr != null)
-      {
-        bindingExpr.UpdateTarget();
-      }
-    }
+        public static void UpdateBindingTarget(this FrameworkElement element, DependencyProperty property)
+        {
+            var bindingExpr = element.GetBindingExpression(property);
+            if (bindingExpr != null)
+            {
+                bindingExpr.UpdateTarget();
+            }
+        }
 
-    public static void UpdateBindingSource(this FrameworkElement element, DependencyProperty property)
-    {
-      var bindingExpr = element.GetBindingExpression(property);
-      if (bindingExpr != null)
-      {
-        bindingExpr.UpdateSource();
-      }
+        public static void UpdateBindingSource(this FrameworkElement element, DependencyProperty property)
+        {
+            var bindingExpr = element.GetBindingExpression(property);
+            if (bindingExpr != null)
+            {
+                bindingExpr.UpdateSource();
+            }
+        }
     }
-  }
 }
