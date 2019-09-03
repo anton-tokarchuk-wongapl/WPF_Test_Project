0b72bed4e33ebdac83eb4a10bd382124dafa17da
Anton Tokarchuk <atokar@softserveinc.com>
2019-09-03 12:05:15 +0300

    Implement behaviours on view.

--- src/SampleWPFProject/ViewModels/ContentFolderViewModel.cs
+++ src/SampleWPFProject/ViewModels/ContentFolderViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using BusinessLogicContracts.Models.ContentModels;
+using ReactiveUI;
 using WPFProject.Helpers.Factories;
 
 namespace WPFProject.ViewModels
@@ -8,12 +9,28 @@ namespace WPFProject.ViewModels
     {
         private readonly ContentBaseViewModelFactory viewModelFactory;
 
+        private bool isSelected;
+
+        private bool isExpanded;
+
         public ContentFolderViewModel(ContentBaseModel Model, ContentBaseViewModel parent) : base(Model, parent)
         {
             viewModelFactory = new ContentBaseViewModelFactory();
 
             var list = viewModelFactory.GetViewModels(this.Model.Children, this);
             _children = new List<ContentBaseViewModel>(list);
+        }
+
+        public bool IsSelected
+        {
+            get => isSelected;
+            set => this.RaiseAndSetIfChanged(ref isSelected, value);
+        }
+
+        public bool IsExpanded
+        {
+            get => isExpanded;
+            set => this.RaiseAndSetIfChanged(ref isExpanded, value);
         }
     }
 }
